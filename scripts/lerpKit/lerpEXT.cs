f08d17b357863de8d84f3a65381e700adb0b9480
Bryan Cancel <bryan.o.cancel@gmail.com>
2018-08-07 11:58:46 -0500

    Update lerpEXT.cs

--- scripts/lerpKit/lerpEXT.cs
+++ scripts/lerpKit/lerpEXT.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using lerpKit;
@@ -7,7 +7,7 @@ using lerpKit;
 
 namespace lerpKit
 {
-    public enum updateLocation { fixedUpdate, Update };
+    public enum updateLocation { fixedUpdate, update };
     public enum unitOfTime { frames, seconds };
     public enum guideDistance { distBetween_Other, distBetween_StartAndEnd, distBetween_CurrAndEnd, distBetween_StartAndCurr };
 }
@@ -51,7 +51,7 @@ public static class lerpEXT
 
     #endregion
 
-    #region Calculate Lerp Value (with Velocity)
+    #region Calculate Lerp Value
 
     public static float calcLerpValue(this float f, float currValue, float endValue, float lerpVelocity_DperF)
     {
@@ -85,53 +85,10 @@ public static class lerpEXT
 
     #endregion
 
-    #region Calculate Lerp Value (with Distance, Time, Unit of Time, and Update Location)
-
-    public static float calcLerpValue(this float f, float currValue, float endValue, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-    {
-        return lerpHelper.calcLerpValue(currValue, endValue, guideDistance, guideTime, UOT_GD, UL);
-    }
-
-    public static float calcLerpValue(this Vector2 v2, Vector2 currVector2, Vector2 endVector2, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-    {
-        return lerpHelper.calcLerpValue(currVector2, endVector2, guideDistance, guideTime, UOT_GD, UL);
-    }
-
-    public static float calcLerpValue(this Vector3 v3, Vector3 currVector3, Vector3 endVector3, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-    {
-        return lerpHelper.calcLerpValue(currVector3, endVector3, guideDistance, guideTime, UOT_GD, UL);
-    }
-
-    public static float calcLerpValue(this Vector4 v4, Vector4 currVector4, Vector4 endVector4, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-    {
-        return lerpHelper.calcLerpValue(currVector4, endVector4, guideDistance, guideTime, UOT_GD, UL);
-    }
-
-    public static float calcLerpValue(this float[] fa, float[] currValues, float[] endValues, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-    {
-        return lerpHelper.calcLerpValue(currValues, endValues, guideDistance, guideTime, UOT_GD, UL);
-    }
-
-    public static float calcLerpValue(this Color c, Color currColor, Color endColor, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-    {
-        return lerpHelper.calcLerpValue(currColor, endColor, guideDistance, guideTime, UOT_GD, UL);
-    }
-
-    #endregion
-
-    #region calcLerpVelocity
-
     public static float calcLerpVelocity(this float f, float guideDistance, float timeToTravel_GD, unitOfTime UOT_GD, updateLocation UL)
     {
         return lerpHelper.calcLerpVelocity(guideDistance, lerpHelper.timeToFrames(timeToTravel_GD, UOT_GD, UL));
     }
-
-    public static float calcLerpVelocity(this float f, float guideDistance, float framesToTravel_GD) //frames of this type (update if we are in update... fixed update if we are in fixed upate)
-    {
-        return guideDistance / framesToTravel_GD;
-    }
-
-    #endregion
 
     public static float distance(this Color c, Color color1, Color color2)
     {
@@ -174,7 +131,7 @@ public static class lerpEXT
 
     #endregion
 
-    #region Calculate Lerp Value (with Velocity)
+    #region Calculate Lerp Value
 
     public static float calcLerpValue(this float currValue, float endValue, float lerpVelocity_DperF)
     {
@@ -208,53 +165,10 @@ public static class lerpEXT
 
     #endregion
 
-    #region Calculate Lerp Value (with Distance, Time, Unit of Time, and Update Location)
-
-    public static float calcLerpValue(this float currValue, float endValue, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-    {
-        return lerpHelper.calcLerpValue(currValue, endValue, guideDistance, guideTime, UOT_GD, UL);
-    }
-
-    public static float calcLerpValue(this Vector2 currVector2, Vector2 endVector2, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-    {
-        return lerpHelper.calcLerpValue(currVector2, endVector2, guideDistance, guideTime, UOT_GD, UL);
-    }
-
-    public static float calcLerpValue(this Vector3 currVector3, Vector3 endVector3, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-    {
-        return lerpHelper.calcLerpValue(currVector3, endVector3, guideDistance, guideTime, UOT_GD, UL);
-    }
-
-    public static float calcLerpValue(this Vector4 currVector4, Vector4 endVector4, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-    {
-        return lerpHelper.calcLerpValue(currVector4, endVector4, guideDistance, guideTime, UOT_GD, UL);
-    }
-
-    public static float calcLerpValue(this float[] currValues, float[] endValues, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-    {
-        return lerpHelper.calcLerpValue(currValues, endValues, guideDistance, guideTime, UOT_GD, UL);
-    }
-
-    public static float calcLerpValue(this Color currColor, Color endColor, float guideDistance, float guideTime, unitOfTime UOT_GD, updateLocation UL)
-    {
-        return lerpHelper.calcLerpValue(currColor, endColor, guideDistance, guideTime, UOT_GD, UL);
-    }
-
-    #endregion
-
-    #region calcLerpVelocity
-
     public static float calcLerpVelocity(this float guideDistance, float timeToTravel_GD, unitOfTime UOT_GD, updateLocation UL)
     {
         return lerpHelper.calcLerpVelocity(guideDistance, lerpHelper.timeToFrames(timeToTravel_GD, UOT_GD, UL));
     }
-
-    public static float calcLerpVelocity(this float guideDistance, float framesToTravel_GD) //frames of this type (update if we are in update... fixed update if we are in fixed upate)
-    {
-        return guideDistance / framesToTravel_GD;
-    }
-
-    #endregion
 
     public static float distance(this Color color1, Color color2)
     {
